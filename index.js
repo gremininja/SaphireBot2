0b30543e814c999b1b3e5a31a8caf4e11d7cae71
gremininja <84020787+gremininja@users.noreply.github.com>
2021-10-31 08:30:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord=require("discord.js");
+
+const client=new Disclord.Client();
+
+client.on("message",message=>{
+    console.log("gelen mesaj"+ message.content)
+
+    if(message.content==="selam")
+    mesage.channel.send("merhaba")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+clinent.login("OTA0MDExMjI3MDUzODUwNjg1.YX1UEA.1i69mM9jX3gcghreN_SZ2d71IvM")
